623f6dbb93d6f45c629122cb77491edd28b811e2
tomz197 <tomas.zifcak197@gmail.com>
2024-12-24 23:42:16 +0100

    fix: refreshing page instead of fetching new question

--- src/components/exam-question-form.tsx
+++ src/components/exam-question-form.tsx
@@ -4,9 +4,21 @@ import { CorrectIncorrect, cn } from "@/lib/utils";
 import { Check, X } from "lucide-react";
 import { useState } from "react";
 
-function ExamQuestionForm({ facts }: { facts: CorrectIncorrect }) {
-  const [selectedFacts, setSelectedFacts] = useState<CorrectIncorrect>([])
-  const [isSubmitted, setIsSubmitted] = useState(false)
+function ExamQuestionForm({
+  facts,
+  resetQuestion: resetQuestionBase,
+}: {
+  facts: CorrectIncorrect;
+  resetQuestion: () => void;
+}) {
+  const [selectedFacts, setSelectedFacts] = useState<CorrectIncorrect>([]);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function resetQuestion() {
+    setSelectedFacts([]);
+    setIsSubmitted(false);
+    resetQuestionBase();
+  }
 
   return (
     <>
@@ -18,14 +30,20 @@ function ExamQuestionForm({ facts }: { facts: CorrectIncorrect }) {
             i !== 0 && " border-t ",
             isSubmitted && (item.isCorrect ? "bg-green-200" : "bg-red-200"),
             isSubmitted ? "cursor-default" : "cursor-pointer",
-            isSubmitted ? (item.isCorrect ? "hover:bg-green-300" : "hover:bg-red-300") : "hover:bg-gray-200",
+            isSubmitted
+              ? item.isCorrect
+                ? "hover:bg-green-300"
+                : "hover:bg-red-300"
+              : "hover:bg-gray-200",
           ])}
           onClick={() => {
-            if (isSubmitted) return
+            if (isSubmitted) return;
             if (selectedFacts.includes(item)) {
-              setSelectedFacts((prev) => prev.filter((fact) => fact.fact !== item.fact))
+              setSelectedFacts((prev) =>
+                prev.filter((fact) => fact.fact !== item.fact),
+              );
             } else {
-              setSelectedFacts((prev) => [...prev, item])
+              setSelectedFacts((prev) => [...prev, item]);
             }
           }}
         >
@@ -38,45 +56,73 @@ function ExamQuestionForm({ facts }: { facts: CorrectIncorrect }) {
           />
 
           <div className="flex-1">
-            <DisplayFact fact={item.fact} correct={item.correct} isSubmitted={isSubmitted} />
+            <DisplayFact
+              fact={item.fact}
+              correct={item.correct}
+              isSubmitted={isSubmitted}
+            />
           </div>
 
-          {isSubmitted && selectedFacts.includes(item) && (
-            item.isCorrect
-              ? <span className="text-green-500"><Check className="w-10 h-10" /></span>
-              : <span className="text-red-500"><X className="w-10 h-10" /></span>
-          )}
+          {isSubmitted &&
+            selectedFacts.includes(item) &&
+            (item.isCorrect ? (
+              <span className="text-green-500">
+                <Check className="w-10 h-10" />
+              </span>
+            ) : (
+              <span className="text-red-500">
+                <X className="w-10 h-10" />
+              </span>
+            ))}
         </div>
       ))}
-      <Button className="w-full mt-4" onClick={() => {
-        if (isSubmitted) {
-          location.reload()
-          return
-        }
-        setIsSubmitted(true)
-      }}>{isSubmitted ? "Další otázka" : "Odeslat"}</Button>
+      <Button
+        className="w-full mt-4"
+        onClick={() => {
+          if (isSubmitted) {
+            resetQuestion();
+            return;
+          }
+          setIsSubmitted(true);
+        }}
+      >
+        {isSubmitted ? "Další otázka" : "Odeslat"}
+      </Button>
     </>
-  )
+  );
 }
 
-function DisplayFact({ fact, correct, isSubmitted }: { fact: string, correct: string | null, isSubmitted: boolean }) {
-  const [showCorrect, setShowCorrect] = useState(false)
+function DisplayFact({
+  fact,
+  correct,
+  isSubmitted,
+}: {
+  fact: string;
+  correct: string | null;
+  isSubmitted: boolean;
+}) {
+  const [showCorrect, setShowCorrect] = useState(false);
 
   return (
     <>
       <p>{showCorrect ? correct : fact}</p>
       {isSubmitted && correct && (
         <div className="flex items-center pt-2 gap-2 select-none">
-          <p onMouseDown={() => setShowCorrect(!showCorrect)} className="font-bold underline italic cursor-pointer">
+          <p
+            onMouseDown={() => setShowCorrect(!showCorrect)}
+            className="font-bold underline italic cursor-pointer"
+          >
             {!showCorrect ? "zobrazit správnou" : "zobrazit nesprávnou"}
           </p>
           <p className="text-gray-600">
-            {!showCorrect ? "(Zobrazuje se nesprávna odpověď)" : "(Zobrazuje se správna odpověď)"}
+            {!showCorrect
+              ? "(Zobrazuje se nesprávna odpověď)"
+              : "(Zobrazuje se správna odpověď)"}
           </p>
         </div>
       )}
     </>
-  )
+  );
 }
 
-export default ExamQuestionForm
+export default ExamQuestionForm;
--- src/routes/exam-part.tsx
+++ src/routes/exam-part.tsx
@@ -1,32 +1,47 @@
 import ExamQuestionForm from "@/components/exam-question-form";
-import { getCorrectIncorrect, getDrill } from "@/lib/utils";
+import { CorrectIncorrect, getCorrectIncorrect, getDrill } from "@/lib/utils";
+import { useCallback, useEffect, useState } from "react";
 import { redirect, useParams } from "react-router-dom";
 
 function ExamPart() {
-  const { section } = useParams()
-  if (!section) {
-    console.error("Section or name not found")
-    redirect("/");
-    return null
-  }
+  const { section } = useParams();
 
-  const drill = getDrill({ section: parseInt(section) })
-  if (!drill) {
-    console.error("Drill not found")
-    redirect("/");
-    return null
-  }
+  const [question, setQuestion] = useState<CorrectIncorrect | null>(null);
 
-  const correctIncorrect = getCorrectIncorrect(drill)
+  const resetQuestion = useCallback(() => {
+    if (!section) {
+      console.error("Section or name not found");
+      redirect("/");
+      return;
+    }
+
+    const drill = getDrill({ section: parseInt(section) });
+    if (!drill) {
+      console.error("Drill not found");
+      redirect("/");
+      return;
+    }
+
+    setQuestion(getCorrectIncorrect(drill));
+  }, [section]);
+
+  useEffect(() => {
+    resetQuestion();
+  }, [resetQuestion]);
+
+  if (!question) return null;
 
   return (
     <>
-      <h2 className="text-xl">Část: {section}: {drill.name}</h2>
+      <h2 className="text-xl">
+        Část: {section}:{" "}
+        {!section ? "" : getDrill({ section: parseInt(section) })?.name ?? ""}
+      </h2>
       <div>
-        <ExamQuestionForm facts={correctIncorrect} />
+        <ExamQuestionForm facts={question} resetQuestion={resetQuestion} />
       </div>
     </>
-  )
+  );
 }
 
-export default ExamPart
+export default ExamPart;
