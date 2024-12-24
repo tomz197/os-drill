743677ab06c8620a9003ddf7eb05b98118d8a80e
tomz197 <tomas.zifcak197@gmail.com>
2024-12-24 23:15:51 +0100

    fix: switch to hash router due to gp pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import Root from "@/routes/root.tsx";
 import Selection from "./routes/selection";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import ExamPart from "./routes/exam-part";
 import ExamAllParts from "./routes/all-exam-parts";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: "/",
