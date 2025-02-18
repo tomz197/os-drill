22a5ce31c5860b40b9a61e7986cfa27bc2dc3144
tomz197 <tomas.zifcak197@gmail.com>
2025-02-18 08:50:31 +0100

    feat: add more questions to credit-part1

--- src/lib/credit-repository/data.ts
+++ src/lib/credit-repository/data.ts
@@ -142,6 +142,146 @@ export const creditData: CreditData = [
         incorrect: "Změna stránkové tabulky nemá žádný vliv na výkon systému.",
         uuid: "de41fb95-921b-4371-8ad9-c79a5733e6e5",
       },
+      {
+        correct:
+          "Virtuální adresa musí být vždy přeložena hardwarovou jednotkou správy paměti na fyzickou adresu.",
+        incorrect:
+          "Virtuální adresa může být přímo využita jako platná fyzická adresa bez jakéhokoli překladu.",
+        uuid: "5fa3e78c-6aaf-444c-b949-d3e06ab3c9e0",
+      },
+      {
+        correct:
+          "Proces s více vlákny sdílí stejný virtuální adresní prostor pro všechna svá vlákna.",
+        incorrect:
+          "Proces s více vlákny má pro každé vlákno zcela oddělený virtuální adresní prostor.",
+        uuid: "abe699fb-248d-4941-95f8-b51dac2e98d1",
+      },
+      {
+        correct:
+          "Stránka velikosti 4 KiB vyžaduje při dvouúrovňovém překladu čtení dvou záznamů stránkových tabulek.",
+        incorrect:
+          "Stránka velikosti 4 KiB potřebuje při dvouúrovňovém překladu pouze jediný záznam stránkové tabulky.",
+        uuid: "cd92eee4-854b-457d-82a5-dd2a984ca589",
+      },
+      {
+        correct:
+          "Jednotka správy paměti využívá TLB k urychlení opakovaných překladů virtuálních adres.",
+        incorrect:
+          "Jednotka správy paměti zpracovává každý překlad adresy vždy bez využití jakékoliv vyrovnávací paměti.",
+        uuid: "e9085dbc-981e-406d-97ff-df59037e27e7",
+      },
+      {
+        correct:
+          "Externí stránkování umožňuje operačnímu systému přesunout nevyužívané stránky paměti do pomalejšího úložiště.",
+        incorrect:
+          "Externí stránkování vyžaduje, aby všechny stránky procesu zůstávaly trvale v rychlé operační paměti.",
+        uuid: "b4d10c16-6b78-4b9f-89eb-d57a6b4113c9",
+      },
+      {
+        correct:
+          "Mechanismus copy on write zabraňuje okamžitému duplikování dat při vzniku nového procesu.",
+        incorrect:
+          "Mechanismus copy on write ihned kopíruje všechny stránky procesu nezávisle na požadavcích zápisu.",
+        uuid: "fc7038a1-3697-48c1-944d-92d6c73a8a6f",
+      },
+      {
+        correct:
+          "Proces může mít více vláken, která společně využívají kód i globální datové struktury.",
+        incorrect:
+          "Proces se skládá z mnoha vláken, ale každé vlákno používá zcela jiný kód a data.",
+        uuid: "740824b2-0d97-4b4a-b9e4-c6fe042a4d02",
+      },
+      {
+        correct:
+          "Fyzické adresy ve starších systémech x86 mohou odkazovat nejen na RAM, ale i na periferní zařízení.",
+        incorrect:
+          "Fyzické adresy v systémech x86 jsou vždy mapovány pouze na operační paměť, nikoli na periferní zařízení.",
+        uuid: "daef6362-f7f6-44d4-9476-083a2991598b",
+      },
+      {
+        correct:
+          "Stránky pro kód běžně sdílejí více procesů, pokud jsou označeny jako pouze pro čtení.",
+        incorrect:
+          "Stránky pro kód jsou vždy privátní a nemohou být nikdy sdíleny mezi odlišnými procesy.",
+        uuid: "2e2b565e-632d-482c-9d99-95f058e7c233",
+      },
+      {
+        correct:
+          "Víceúrovňové stránkové tabulky šetří paměť označením nevyužívaných segmentů jako nepřítomné bez alokace dalšího prostoru.",
+        incorrect:
+          "Víceúrovňové stránkové tabulky pokaždé alokují všechny nižší úrovně tabulek i pro nevyužité virtuální adresy.",
+        uuid: "ca6e8cab-28a3-407b-82ce-2e69b229440b",
+      },
+      {
+        correct:
+          "Změna registru ukazujícího na tabulku první úrovně je klíčová při přepínání mezi procesy.",
+        incorrect:
+          "Při přepínání procesů zůstává vždy stejný registr ukazující na tabulku první úrovně.",
+        uuid: "56da73d3-0ace-40c4-9754-b521ed3b1e5a",
+      },
+      {
+        correct:
+          "Mapování dvou virtuálních adres na stejný rámec umožňuje vzájemnou komunikaci procesů prostřednictvím sdílené paměti.",
+        incorrect:
+          "Mapování dvou virtuálních adres na stejný rámec znemožňuje procesům jakoukoli vzájemnou komunikaci.",
+        uuid: "1df981c2-d9e2-4187-98c9-810d07cd8335",
+      },
+      {
+        correct:
+          "Stránka označená příznakem pouze pro čtení může být následně kopírována při pokusu o zápis.",
+        incorrect:
+          "Stránka označená příznakem pouze pro čtení vždy brání zápisu bez možnosti pozdějšího překopírování dat.",
+        uuid: "3e2eb134-9a06-45bd-9cea-bf486e046d2a",
+      },
+      {
+        correct:
+          "Zneplatnění TLB při přepnutí procesu dočasně zpomaluje překlad adres, dokud se neobnoví potřebné záznamy.",
+        incorrect:
+          "Zneplatnění TLB nikdy neovlivňuje výkon překladů adres, protože se okamžitě obnoví všechny potřebné položky.",
+        uuid: "9162bcbc-dbb5-4194-a3b3-3276bb919fd4",
+      },
+      {
+        correct:
+          "Líné načítání při startu programu načítá stránky z disku až teprve při jejich prvním použití.",
+        incorrect:
+          "Líné načítání vždy předem načte celý spustitelný soubor do operační paměti bez ohledu na potřebu.",
+        uuid: "3bfcfe1d-968d-4379-96bd-da30b5b01157",
+      },
+      {
+        correct:
+          "Přístup k paměti pomocí instrukcí ld a st lze rozložit na sekvenci jednoduchých čtení a zápisů.",
+        incorrect:
+          "Instrukce ld a st nelze nikdy nahradit sekvencí elementárních operací čtení a zápisu registrů.",
+        uuid: "df1628db-d585-471f-8e74-c0f25e5a06cd",
+      },
+      {
+        correct:
+          "Větší datové struktury leží v paměti na sousedních adresách, pokud nezasahují do neplatného prostoru.",
+        incorrect:
+          "Větší datové struktury se běžně ukládají roztříštěně napříč nesouvisejícími oblastmi virtuální paměti.",
+        uuid: "ce998f62-d772-4e77-a915-5eba83468ee9",
+      },
+      {
+        correct:
+          "Některé stránky v paměti procesu mohou reprezentovat paměť periferních zařízení, místo klasické RAM.",
+        incorrect:
+          "Stránky v paměti procesu jsou vždy mapovány výhradně do oblastí klasické RAM a nikde jinde.",
+        uuid: "ee2014ee-c292-4b00-97e6-dd4c0833ea29",
+      },
+      {
+        correct:
+          "Virtualizace paměti dovoluje spustit několik procesů se stejnými virtuálními adresami, ale odlišnými fyzickými umístěními.",
+        incorrect:
+          "Virtualizace paměti předepisuje, že žádné dva procesy nemohou sdílet stejné číselné hodnoty virtuálních adres.",
+        uuid: "47445754-da05-4893-9cf1-0c78bbfcc020",
+      },
+      {
+        correct:
+          "Při mapování datového souboru do paměti se obsah stránek načítá z disku až při jejich použití.",
+        incorrect:
+          "Mapování souboru do paměti vždy vyžaduje jednorázové zkopírování veškerého obsahu do virtuálního adresního prostoru.",
+        uuid: "dccc7063-1ed4-4008-a16e-c2d6715c6d88",
+      },
     ],
   },
 ];
